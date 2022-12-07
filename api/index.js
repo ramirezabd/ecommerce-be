d3925034252595c925e0ef0e8ac54985a1d39ec6
Fajar Abdurrahman <kamentosa@gmail.com>
2022-12-07 09:00:06 +0700

    timpa conflict

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
+import { bodyParser } from "body-parser";
+import cors from "cors";  
+import dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
 import helmet from "helmet";
 import multer from "multer";
 import path from "path";
-import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import routes from "../src/routes/routes";
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
+app.use(bodyParser.json());
 
 // UniqueSuffix sebagai randomizer
 const ImgStorage = multer.diskStorage({
@@ -66,9 +68,7 @@ app.use(
   ]),
 );
 
-app.get("/", (req, res) => {
-  res.send("ok");
-});
+
 app.use("/api/v1", routes);
 
 export default app;
--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
+/* eslint-disable import/no-dynamic-require */
+import { readdirSync } from "fs";
+import { basename as _basename, join } from "path";
+import Sequelize, { DataTypes } from "sequelize";
+import { env as _env } from "process";
 
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-// eslint-disable-next-line import/no-dynamic-require
+const basename = _basename(__filename);
+const env = _env.NODE_ENV || "development";
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(_env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -21,7 +21,7 @@ if (config.use_env_variable) {
   );
 }
 
-fs.readdirSync(__dirname)
+readdirSync(__dirname)
   .filter((file) => {
     return (
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
@@ -29,10 +29,7 @@ fs.readdirSync(__dirname)
   })
   .forEach((file) => {
     // eslint-disable-next-line import/no-dynamic-require, global-require
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes,
-    );
+    const model = require(join(__dirname, file))(sequelize, DataTypes);
     db[model.name] = model;
   });
 
@@ -45,5 +42,4 @@ Object.keys(db).forEach((modelName) => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-
-module.exports = db;
+export default db;
